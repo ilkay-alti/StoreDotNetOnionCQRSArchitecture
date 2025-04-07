326e280ab082ad2346f4eff96e9ca4f96699d5c5
ilkay-alti <ilkayalti@hotmail.com>
2025-04-07 04:09:24 -0700

    Custom AutoMapper development

---
+++ Core/StoreOnionArchitecture.Application/Interfaces/AutoMapper/IMapper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreOnionArchitecture.Application.Interfaces.AutoMapper
+{
+    public interface IMapper
+    {
+        TDestination Map<TDestination, TSource>(TSource source, string? ignore = null);
+
+        //list
+        IList<TDestination>Map<TDestination,TSource>(IList<TSource> source, string? ignore = null);
+        // obj
+        TDestination Map<TDestination>(object source, string? ignore = null);
+        TDestination Map<TDestination>(IList<object> source, string? ignore = null);
+
+    }
+}
+
+
+//maper.Map<BrandDTO,Brand>(brand) return BrandDto
+//maper.Map<Brand,BrandDTO>(brand) return Brand
