92b59567746ab6a580d741551d5eb9910f0d537b
ilkay-alti <ilkayalti@hotmail.com>
2025-04-08 09:07:38 -0700

    add  change Db by  IdentityDbContext and User Role

---
+++ Core/StoreOnionArchitecture.Domain/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace StoreOnionArchitecture.Domain.Entities
+{
+    public class User : IdentityUser<Guid>
+    {
+        public string FullName { get; set; }
+        public string? RefleshToken { get; set; }
+        public DateTime? RefleshTokenExpiryTime { get; set; }
+    }
+}
