4322d9057454be9a99e04fd6d3899c6478947bf3
ilkay-alti <ilkayalti@hotmail.com>
2025-04-08 09:32:58 -0700

    Access and Refresh Token Services

---
+++ Core/StoreOnionArchitecture.Application/Interfaces/Tokens/ITokenService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StoreOnionArchitecture.Domain.Entities;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace StoreOnionArchitecture.Application.Interfaces.Tokens
+{
+    public interface ITokenService
+    {
+        Task<JwtSecurityToken> CreateToken(User user, IList<string> roles);
+        string GenerateRefleshToken();
+        ClaimsPrincipal? GetPrincipalFromExpiredToken(string token);
+
+    }
+}
---
+++ Infrastructure/StoreOnionArchitecture.Infrastructure/Registration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using StoreOnionArchitecture.Infrastructure.Tokens;
+
+namespace StoreOnionArchitecture.Infrastructure
+{
+    public static class Registration
+    {
+        public static void  AddInfrastructureRegistration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<TokenSettings>(options => configuration.GetSection("JWT"));
+
+        }
+
+
+    }
+}
